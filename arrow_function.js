4c6559f729cf84faeb4c8b6c21f3af6750f65258
mtouqeerzia38-lab <mtouqeerzia38@gmail.com>
2026-04-15 19:05:02 +0500

    Comment out demo code and add map/filter examples

--- arrow_function.js
+++ arrow_function.js
@@ -1,14 +1,36 @@
-const a = 'hello';
-const b = 'touqeer';
+// const a = 'hello';
+// const b = 'touqeer';
 
-const concate = (a , b) => {
-    return a + b;
-}
-const result = concate(10 , 15)
-document.getElementById('demo').innerHTML = result;
+// const concate = (a , b) => {
+//     return a + b;
+// }
+// const result = concate(10 , 15)
+// document.getElementById('demo').innerHTML = result;
  
-const demo1 = (name) => {
-    document.getElementById('demo1').innerHTML = name;
-}
+// const demo1 = (name) => {
+//     document.getElementById('demo1').innerHTML = name;
+// }
 
-console.log = demo1('ahmad');
+// console.log = demo1('ahmad');
+
+
+// array map///////////////////////////////////?
+
+
+
+// let arr = [ 30 , 45 , 55 , 12 , 4 , 6];
+
+// let item = arr.map((value , index , array)=>{
+//     return ` value:${value}, index:${index}`;
+// })
+// document.getElementById('demo').innerHTML = item.join('<br/>');
+
+
+
+// array map///////////////////////////////////?
+
+
+// const filter = arr.filter((value)=>{
+//     return value>10;
+// })
+// document.getElementById('demo1').innerHTML =filter.join('<br/>');
